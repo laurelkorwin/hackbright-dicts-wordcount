b8a7db30dc54d8de2be74b3919c3071d005ba9f1
Hackbright Student <info@hackbrightacademy.com>
2016-07-11 12:44:18 -0700

    updated function

--- wordcount.py
+++ wordcount.py
@@ -1,20 +1,20 @@
 # put your code here.
 def word_count(file):
-    poem = open(file)
-    word_counts = {}
+    with open(file) as poem:
+        word_counts = {}
 
-    for line in poem:
-        line = line.rstrip()
-        line = line.split(' ')
-        for word in line:
-            word_counts[word] = word_counts.get(word, 0) + 1
+        for line in poem:
+            line = line.rstrip()
+            line = line.split(' ')
+            for word in line:
+                word_counts[word] = word_counts.get(word, 0) + 1
 
     def get_count(dct):
-        for word, count in dct.items():
+        for word, count in dct.iteritems():
             print word, count
 
     get_count(word_counts)
 
-    poem.close()
-
 word_count('test.txt')
+word_count('twain.txt')
+
